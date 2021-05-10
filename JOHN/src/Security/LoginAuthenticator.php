17e6001f5621a5878d9000450d74e310cd71419a
john-btg <71494892+john-btg@users.noreply.github.com>
2021-05-10 23:51:28 +0200

    Update LoginAuthenticator.php

--- JOHN/src/Security/LoginAuthenticator.php
+++ JOHN/src/Security/LoginAuthenticator.php
@@ -97,9 +97,9 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator implements Passw
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-//        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-        //return new RedirectResponse($this->urlGenerator->generate('TestApi'));
-        return $this->json(["data"=>'user_connected']);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('TestApi'));
+        //return $this->json(["data"=>'user_connected']);
     }
 
     protected function getLoginUrl()
